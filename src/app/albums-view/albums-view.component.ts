9208b45d5ea97d2f881d207329b46158dbe51a1a
Charles de Vandiere <charles.de.vandiere@outlook.fr>
2018-05-09 19:02:11 +0200

    création du layout

---
+++ src/app/albums-view/albums-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-albums-view',
+  templateUrl: './albums-view.component.html',
+  styleUrls: ['./albums-view.component.scss']
+})
+export class AlbumsViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,27 @@ import { NgModule } from '@angular/core';
 import { NgxElectronModule } from 'ngx-electron';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './controls/button/button.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { NavButtonComponent } from './controls/nav-button/nav-button.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { CollectionViewComponent } from './collection-view/collection-view.component';
+import { AlbumsViewComponent } from './albums-view/albums-view.component';
 
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        ButtonComponent,
+        MainViewComponent,
+        NavButtonComponent,
+        CollectionViewComponent,
+        AlbumsViewComponent
     ],
     imports: [
         BrowserModule,
-        NgxElectronModule
+        NgxElectronModule,
+        AppRoutingModule
     ],
     providers: [],
     bootstrap: [AppComponent]
